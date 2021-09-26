964e91e2fdc56997bb007df2debdd65dbd8379db
Paulo Cesar <paulo-cesar-batista@hotmail.com>
2021-09-26 09:11:16 -0300

    modulo 3

---
+++ Desktop/workspace/primeiro-projeto/src/components/TesteEvento.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="teste-evento">
+        <h2>Teste evento</h2>´
+       <p v-on:click = "deixarMaiscula">{{mensagem}}</p>
+    </div>
+</template>
+	
+<script>
+export default{    
+    name: 'TesteEvento',
+    data(){
+        return{
+            mensagem: 'o rato roeu a roupa do rei de roma'
+        }
+    },
+    methods: {
+        deixarMaiscula(){
+            this.mensagem = this.mensagem.toUpperCase();
+        }
+    }
+}
+</script>
+
+<style scoped>
+</style>
